29b81cdc24c77ad8c81cf14758080e24864ee8cf
Gustavo Thomaz <gutomazz@gmail.com>
2022-06-02 11:11:40 -0300

    Finalizando o jogo

--- main.js
+++ main.js
@@ -5,19 +5,31 @@ const jump = ()=>{
     mario.classList.add('jump');
 
     setTimeout(()=>{
+
         mario.classList.remove('jump');
     }, 500);
 }
 
 const loop = setInterval(() => {
 
-    const pipePosition = pipe.offsetleft(); 
+    const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    console.log(marioPosition);
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
 
         pipe.style.animation = 'none';
-        pipe.style.left = '${pipePosition}px';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = '/css/game-over.png';
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop);
     }
 
 }, 10);
